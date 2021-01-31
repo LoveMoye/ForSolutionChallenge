7e1e7829175ce2e47199470422c2aee591e5fb4f
LoveMoye <jwy06048@gmail.com>
2021-01-31 21:55:30 +0900

    Using Chartjs

--- script.js
+++ script.js
@@ -31,19 +31,20 @@ function deactivateAll() {
 
 function imgRenderer() {
     const people = [...(document.getElementsByClassName('img_person'))];
+    let statContainer = document.getElementsByClassName('statContainer');
+    let rect;
     let criterion = 0;
+    let unit = window.innerHeight*(0.8-0.3)/20;
+    let prev = window.innerHeight * 0.8;
     if(people !== null) {
         window.addEventListener("scroll", e => {
-            let statContainer = document.getElementsByClassName('statContainer');
-            let rect = statContainer[0].getBoundingClientRect();
-            let unit = window.innerHeight*(0.8-0.3)/20;
-            let prev = window.innerHeight * 0.8;
+            rect = statContainer[0].getBoundingClientRect();
             if(rect.top < window.innerHeight * 0.8 && 
                 rect.top >= window.innerHeight * 0.4) {
                 criterion = Math.ceil((prev - rect.top) / unit);
                 deactivateAll();
                 activatePart(criterion);
-            } else if(rect.top >= window.innerHeight * 0.8) {
+            } else if(rect.top >= window.innerHeight * 0.8 || rect.top < -250) {
                 deactivateAll();
             } else {
                 activateAll();
@@ -57,14 +58,15 @@ imgRenderer();
 
 function chartDraw() {
     let duration = 1500;
+    let statContainer = document.getElementsByClassName('statContainer');
+    let rect;
+    // console.log("rect.top", rect.top);
+    // console.log(rect.width, rect.height);
+    // console.log("window.innerHeight", window.innerHeight*0.85);
+    const canvas = document.getElementById("chart1");
+    const ctx = canvas.getContext("2d");
     window.addEventListener("scroll", e => {
-        let statContainer = document.getElementsByClassName('statContainer');
-        let rect = statContainer[1].getBoundingClientRect();
-        // console.log("rect.top", rect.top);
-        // console.log(rect.width, rect.height);
-        // console.log("window.innerHeight", window.innerHeight*0.85);
-        const canvas = document.getElementById("chart");
-        const ctx = canvas.getContext("2d");
+        rect = statContainer[1].getBoundingClientRect()
         if(rect.top < window.innerHeight*2/3 && rect.top >= -150) {
             let pieChart = new Chart(ctx, {
                 type: 'pie',
@@ -94,3 +96,117 @@ function chartDraw() {
 }
 
 chartDraw();
+
+function chartDraw2() {
+    let duration = 2000;
+    let statContainer = document.getElementsByClassName('statContainer');
+    let rect;
+    // console.log("rect.top", rect.top);
+    // console.log(rect.width, rect.height);
+    // console.log("window.innerHeight", window.innerHeight*0.85);
+    const canvas = document.getElementById("chart2");
+    const ctx = canvas.getContext("2d");
+    window.addEventListener("scroll", e => {
+        rect = statContainer[2].getBoundingClientRect();
+        if(rect.top < window.innerHeight*2/3 && rect.top >= -150) {
+            let barChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    datasets: [{
+                        data: [1.8, 18.3, 46.0, 33.9],
+                        // backgroundColor: [
+                        //     "#C1C1C1", "#8E8E8E"
+                        // ],
+                        label: "우리나라의 장애인에 대한 차별 인식(전체)"
+                    }],
+                    labels: [
+                        '전혀 없다', '별로 없다', '약간 있다', '매우 있다'
+                    ]
+                },
+                options: {
+                    animation: {
+                        duration: duration
+                    },
+                    scales: {
+                        xAxes: [{
+                            gridLines: {
+                                display: false
+                            }
+                        }],
+                        yAxes: [{
+                            gridLines: {
+                                display: false
+                            },
+                            ticks: {
+                                display: false
+                            }
+                        }]
+                    }
+                }
+            })
+            duration = 0;
+        } else if (rect.top > window.innerHeight*0.85 || rect.top < -350) {
+            ctx.clearRect(0,0,rect.width, rect.height);
+            duration = 1500;
+        }
+    })
+}
+
+chartDraw2();
+
+function chartDraw3() {
+    let duration = 2000;
+    let statContainer = document.getElementsByClassName('statContainer');
+    let rect;
+    // console.log("rect.top", rect.top);
+    // console.log(rect.width, rect.height);
+    // console.log("window.innerHeight", window.innerHeight*0.85);
+    const canvas = document.getElementById("chart3");
+    const ctx = canvas.getContext("2d");
+    window.addEventListener("scroll", e => {
+        rect = statContainer[3].getBoundingClientRect();
+        if(rect.top < window.innerHeight*2/3 && rect.top >= -150) {
+            let barChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    datasets: [{
+                        data: [2.5, 33.3, 54.3, 9.9],
+                        // backgroundColor: [
+                        //     "#C1C1C1", "#8E8E8E"
+                        // ],
+                        label: "장애등록 후 국가나 사회로부터의 지원에 대한 인식(전체)"
+                    }],
+                    labels: [
+                        '매우 많음', '약간 받고 있음', '별로 받지 못하고 있음', '전혀 받지 못하고 있음'
+                    ]
+                },
+                options: {
+                    animation: {
+                        duration: duration
+                    },
+                    scales: {
+                        xAxes: [{
+                            gridLines: {
+                                display: false
+                            }
+                        }],
+                        yAxes: [{
+                            gridLines: {
+                                display: false
+                            },
+                            ticks: {
+                                display: false
+                            }
+                        }]
+                    }
+                }
+            })
+            duration = 0;
+        } else if (rect.top > window.innerHeight*0.85 || rect.top < -350) {
+            ctx.clearRect(0,0,rect.width, rect.height);
+            duration = 1500;
+        }
+    })
+}
+
+chartDraw3();
